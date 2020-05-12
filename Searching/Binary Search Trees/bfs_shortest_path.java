54ccb5a7327ffc8a83f1e12bb0fb9271a1d980c5
nuistzhou <nuistzhou@gmail.com>
2020-05-12 21:10:54 +0200

    Create bfs_shortest_path.java

---
+++ Searching/Binary Search Trees/bfs_shortest_path.java
@@ -0,0 +1,25 @@
+public class BFS<Node root, Node target> {
+    public BFS(Node root, Node target
+    {
+        Queue<Node> queue;
+        int step = 0;
+        queue.add(root);
+        
+        while (not queue.isEmpty):
+            {
+                step += 1;
+                int q_size = queue.size();
+                for (int i=0; i<=q_size; i++)
+                {
+                    Node curr = queue.peek();
+                    if (curr == target):
+                        return step;
+                    for (Node next : the neighbors of cur) {
+                        add next to queue;
+                    }
+                }
+                queue.pop();
+            }
+        return -1
+    }
+}
